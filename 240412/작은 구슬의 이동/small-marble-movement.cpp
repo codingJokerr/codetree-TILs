bae775689071173caa33db6884e59be59cb90eb3
codingJokerr <126440492+codingJokerr@users.noreply.github.com>
2024-04-12 17:11:18 +0900

    Title: 작은 구슬의 이동, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240412/작은 구슬의 이동/small-marble-movement.cpp
+++ 240412/작은 구슬의 이동/small-marble-movement.cpp
@@ -3,63 +3,52 @@ using namespace std;
 
 #define DIR_NUM 4
 
+// 동서남북 == RLDU
 int dx[DIR_NUM] = { 1, -1, 0, 0 };
 int dy[DIR_NUM] = { 0, 0, -1, 1 };
 
+int n, t;
+int r, c;
+char d;
+int dir = 0;
 
 int main() {
-	int n, t;
-	int r, c;
-	char d;
 	cin >> n >> t >> r >> c >> d;
 
-	// 방향 지정하기
-	int dir;
-	if (d == 'U')
-		dir = 3;
-	else if (d == 'D')
-		dir = 2;
-	else if (d == 'R')
-		dir = 0;
-	else
-		dir = 1;
+	// dir값 지정
+	if (d == 'R') dir = 0;
+	else if (d == 'L') dir = 1;
+	else if (d == 'D') dir = 2;
+	else if (d == 'U') dir = 3;
 
-	while (t) {
-		// 벽에 닿는 경우(1이거나 n인 경우) => 방향 뒤집기
-		// U,D r가 3 2
-		// R,L c가 0 1
+	for (int i = 0; i < t; i++) {// t만큼 반복한다.
+	
 
-		if (dir == 3 || dir == 2) {// 위 아래
-			r += dy[dir];
-			if (r == 1) {
-				// 방향 반대로 바꾸기
-				if (dir == 3) { dir--; }
-				else { dir++; }
-			}
-			else if (r == n) {
-				// 방향 반대로 바꾸기
-				if (dir == 3) { dir--; }
-				else { dir++; }
-			}
-			
-		}
-
-		if (dir == 1 || dir == 0) {// 오 왼
+		if (dir == 0 || dir == 1) {// c값 변환
 			c += dx[dir];
-			if (c == 1) {
-				// 방향 반대로 바꾸기
-				if (dir == 1) { dir--; }
-				else dir++;
-			}
-			if (c == n) {// 오 왼
-				// 방향 반대로 바꾸기
-				if (dir == 1) { dir--; }
-				else dir++;
-			}
-			
-		}
 
-		cout << t << ": " << c << " " << t << endl;
+			if (c == 1 || c == n) {
+				i++;
+				//cout << "i값이 +1됨" << endl;
+				if (dir == 0) { dir++; }
+				else if (dir == 1) { dir--; }
+			}
+
+			
+			//cout << "r값: " << r << endl;
+		}
+		else if (dir == 2 || dir == 3) {// r값 변환
+			r += dy[dir];
+
+			if (r == 1 || r == n) {
+				i++;
+				if (dir == 2) { dir++; }
+				else if (dir == 3) { dir--; }
+			}
+
+			
+			//cout << "c값: " << c << endl;
+		}
 	}
 	cout << r << " " << c;
 }
