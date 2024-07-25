7e6770eb18775697955fed1874b8841056722be5
codingJokerr <126440492+codingJokerr@users.noreply.github.com>
2024-07-25 16:10:49 +0900

    Title: 별 찍는 것을 5번 반복하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240725/별 찍는 것을 5번 반복하기/repeat-shooting-the-stars-five-times.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+void Print5stars(){
+    for(int i=0; i<5; i++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+
+int main() {
+   for(int i=0; i<4; i++){
+    Print5stars();
+   }
+}
