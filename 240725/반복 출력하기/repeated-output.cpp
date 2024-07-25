6c0e367fac4a489cd992a4091d8ce8ffc2526839
codingJokerr <126440492+codingJokerr@users.noreply.github.com>
2024-07-25 16:33:19 +0900

    Title: 반복 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240725/반복 출력하기/repeated-output.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+void PrintN(int n){
+    for(int i=0; i<n; i++)
+        cout<<"12345^&*()_"<<endl;
+}
+
+int main() {
+    int row_num;
+    cin>> row_num;
+
+    PrintN(row_num);
+}
