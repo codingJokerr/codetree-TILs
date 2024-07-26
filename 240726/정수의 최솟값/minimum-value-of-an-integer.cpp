e48059a7b833e44f082a011a7bd3d193bef0f980
codingJokerr <126440492+codingJokerr@users.noreply.github.com>
2024-07-26 18:15:28 +0900

    Title: 정수의 최솟값, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240726/정수의 최솟값/minimum-value-of-an-integer.cpp
+++ 240726/정수의 최솟값/minimum-value-of-an-integer.cpp
@@ -1,15 +1,26 @@
 #include <iostream>
-#include <algorithm>
+// #include <algorithm>
+
 using namespace std;
 
-int Compare(int a, int b, int c) {
-    int fmin = min(a, b);
-    int real_min = min(fmin, c);
-    return real_min;
+int Min(int a, int b, int c) {
+    int min_val=a;
+    if(min_val >b)
+        min_val = b;
+    if(min_val >c)
+        min_val = c;
+
+    return min_val;
 }
+
+// int Compare(int a, int b, int c) {
+//     int fmin = min(a, b);
+//     int real_min = min(fmin, c);
+//     return real_min;
+// }
 
 int main() {
     int a, b, c; cin>>a>>b>>c;
 
-    cout << Compare(a, b, c);
+    cout << Min(a, b, c);
 }
