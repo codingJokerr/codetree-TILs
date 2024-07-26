e60374e389f10ab8179fe9cc83940dbe78c1e3d2
codingJokerr <126440492+codingJokerr@users.noreply.github.com>
2024-07-26 15:39:23 +0900

    Title: 최대공약수 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240726/최대공약수 구하기/find-the-greatest-common-divisor.cpp
+++ 240726/최대공약수 구하기/find-the-greatest-common-divisor.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 void Gcdivision(int n, int m) {
     int gcd=1;
-    for(int i=2; i<100; i++){
+    for(int i=2; i<=100; i++){
         if(n%i==0){
             if(m%i==0){
                 if(gcd<i)
