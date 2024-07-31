b8f263f74bac49d6831caca2b5b4f72c555586fe
codingJokerr <126440492+codingJokerr@users.noreply.github.com>
2024-07-31 17:08:26 +0900

    Title: 2021년 날짜의 유무, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240731/2021년 날짜의 유무/with-or-without-2021.cpp
+++ 240731/2021년 날짜의 유무/with-or-without-2021.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-int Day[13]={-1, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+int Day[13]={-1, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
 bool IsExist(int M, int D) {
     if(M > 12) return false;
