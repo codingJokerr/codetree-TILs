8664a307b55ed993865fa8820c9bd790d02331af
codingJokerr <126440492+codingJokerr@users.noreply.github.com>
2024-08-08 02:20:02 +0900

    Title: 1이 되는 순간까지, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240808/1이 되는 순간까지/until-the-moment-I-reach-one.cpp
+++ 240808/1이 되는 순간까지/until-the-moment-I-reach-one.cpp
@@ -1,27 +1,19 @@
 #include <iostream>
 using namespace std;
 
-int cnt=0;
+// n에서 시작하여 1이 되기위해 거쳐야하는 횟수를 반환하는 함수
+int GetNum(int n) {
+    if(n==1)    // 1이면 더이상 진행할 작없이 없으므로 더 필요한 횟수는 0이다.
+        return 0;
 
-int MomentOfOne(int n) {
-    if(n==1) {
-        return 1;
-    }
-
-    if(n%2==0) {
-        cnt++;
-        return MomentOfOne(n/2);
-    }
-    else {
-        cnt++;
-        return MomentOfOne(n/3);
-    }
+    if(n%2==0)  // 2로 나눠 진행했을 때의 횟수 + 1번
+        return GetNum(n/2) +1;
+    else        // 3으로 나눠 진행했을 때의 횟수 +1번
+        return GetNum(n/3) +1;
 }
 
 int main() {
     int n;  cin>>n;
 
-    MomentOfOne(n);
-
-    cout<<cnt;
+    cout<<GetNum(n);
 }
