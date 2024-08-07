5716daf8e0dfca6191424e88231ef81bbf559bf8
codingJokerr <126440492+codingJokerr@users.noreply.github.com>
2024-08-07 16:38:39 +0900

    Title: 특정 구간의 합, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240807/특정 구간의 합/sum-of-specific-intervals.cpp
+++ 240807/특정 구간의 합/sum-of-specific-intervals.cpp
@@ -2,25 +2,30 @@
 using namespace std;
 
 #define MAX_N 100
-int A[MAX_N+1];
+#define MAX_M 100
+int A[MAX_N + 1];
+int a1[MAX_M], a2[MAX_M];
 
 int GetAnswer(int a1, int a2) {
-    int sum=0;
-    for(int i=a1; i<=a2; i++) {
-        sum+=A[i];
-    }
-    return sum;
+	int sum = 0;
+	for (int i = a1; i <= a2; i++) {
+		sum += A[i];
+	}
+	return sum;
 }
 
 int main() {
-    int n,m,a1,a2;    cin>>n>>m;
+	int n, m;    cin >> n >> m;
 
-    for(int i=1; i<n+1; i++) {
-        cin>>A[i];
-    }
+	for (int i = 1; i < n + 1; i++) {
+		cin >> A[i];
+	}
 
-    for(int i=0; i<m; i++) {
-        cin>>a1>>a2;
-        cout<<GetAnswer(a1,a2)<<endl;
-    }
+	for (int i = 0; i < m; i++) {
+		cin >> a1[i] >> a2[i];
+	}
+
+	for (int i = 0; i < m; i++) {
+		cout << GetAnswer(a1[i], a2[i]) << endl;
+	}
 }
