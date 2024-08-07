c75c3130affda70aad21f2a4cd47805d015d5020
codingJokerr <126440492+codingJokerr@users.noreply.github.com>
2024-08-07 17:55:55 +0900

    Title: 반복 출력하기 2, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

--- 240807/반복 출력하기 2/repeated-output-2.cpp
+++ 240807/반복 출력하기 2/repeated-output-2.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 using namespace std;
 
+// 1부터 n번째 줄까지 문자열을 출력하는 함수
 void Print(int n) {
     if(n==0) // 종료 조건
         return;
 
-    Print(n-1); // 동일한 함수 f를 다시 이용
+    Print(n-1); // 1부터 n-1번째 줄까지 출력하는 함수
+    // 동일한 함수 f를 다시 이용
 
     cout<<"HelloWorld\n";   // 함수의 내용
 }
