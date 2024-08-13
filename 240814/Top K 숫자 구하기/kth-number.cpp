dea1bb17bc76fc9e277b9d9f584c06982afa62d7
codingJokerr <126440492+codingJokerr@users.noreply.github.com>
2024-08-14 00:45:25 +0900

    Title: Top K 숫자 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240814/Top K 숫자 구하기/kth-number.cpp
+++ 240814/Top K 숫자 구하기/kth-number.cpp
@@ -3,16 +3,16 @@
 
 using namespace std;
 
-#define MAX_N 1001
+#define MAX_N 1000
 int arr[MAX_N];
 
 int main() {
-    int n,k;  cin>>n>>k;
+    int n,k;    cin>>n>>k;
 
-    for(int i=1; i<=n; i++)
-        cin>>arr[n];
+    for(int i=0; i<n; i++) 
+        cin>>arr[i];
 
-    sort(arr, arr+n+1);
+    sort(arr, arr+n);
 
-    cout<<arr[k];
+    cout<<arr[k-1];
 }
