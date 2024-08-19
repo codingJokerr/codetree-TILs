aec6df78c1823ea5904d8bef7551557257b66cdb
codingJokerr <126440492+codingJokerr@users.noreply.github.com>
2024-08-19 23:36:17 +0900

    Title: 코드네임, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240819/코드네임/code-name.cpp
+++ 240819/코드네임/code-name.cpp
@@ -17,16 +17,16 @@ int main() {
     Agent agents[5];
 
     string name;    int score;
-    string Mname="";   int Mscore=101;
     for(int i=0; i<5; i++) {
         cin>>name>>score;
         agents[i] = Agent(name, score);
-
-        if(Mscore>score){
-            Mscore = score;
-            Mname = name;
-        }
     }
 
-    cout<<Mname<<" "<<Mscore;
+    int min_idx = 0;
+    for(int i=1; i<5; i++) {
+        if(agents[min_idx].score > agents[i].score)
+            min_idx = i;
+    }
+
+    cout<<agents[min_idx].name<<" "<<agents[min_idx].score;
 }
