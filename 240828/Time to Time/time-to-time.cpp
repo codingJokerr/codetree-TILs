dc8063687711a35281e500c86f95107c8aa41611
codingJokerr <126440492+codingJokerr@users.noreply.github.com>
2024-08-28 23:34:25 +0900

    Title: Time to Time, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240828/Time to Time/time-to-time.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c, d;
+    cin>>a>>b>>c>>d;
+
+    int first = a*60 + b;
+    int second = c*60 + d;
+
+    cout<<second - first;
+}
