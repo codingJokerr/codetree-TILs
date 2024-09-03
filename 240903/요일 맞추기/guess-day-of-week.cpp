2d369a21e8f4aab7a5386b46a217e7a8ae055fc6
codingJokerr <126440492+codingJokerr@users.noreply.github.com>
2024-09-03 16:22:55 +0900

    Title: 요일 맞추기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240903/요일 맞추기/guess-day-of-week.cpp
+++ 240903/요일 맞추기/guess-day-of-week.cpp
@@ -2,54 +2,53 @@
 using namespace std;
 
 int main() {
-    // m1월 d1일 ~ m2월 d2일까지의 날짜 차 구한 후 %7의 값에 따라 요일 구하기
+	// m1월 d1일 ~ m2월 d2일까지의 날짜 차 구한 후
+	// %7의 값에 따라 요일 구하기
+	int m1, m2, d1, d2;
+	cin >> m1 >> d1 >> m2 >> d2;
 
-    int m1, m2, d1, d2;
-    cin>>m1>>d1>>m2>>d2;
+	int month[13] = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 
-    int month[13] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	int days1 = d1;
+	for (int i = 1; i < m1; i++) {
+		days1 += month[i];
+	}
 
-    int days1 = d1;
-    for(int i=1; i<m1; i++) {
-        days1 += month[i];
+	int days2 = d2;
+	for (int i = 1; i < m2; i++) {
+		days2 += month[i];
+	}
+
+	int dif = days2 - days1;
+	while(dif < 0) {
+        dif += 7;
     }
 
-    int days2 = d2;
-    for(int i=1; i<m2; i++) {
-        days2 += month[i];
-    }
+	//cout << "*** dif " << dif;
 
-    int dif=0;
-    if(days2 > days1) 
-        dif = days2 - days1;
-    else
-        dif = days2 - days1 + 7;
+	int ans = dif % 7;
+	//cout << "\n*** ans " << ans << endl;
 
-    //cout<<"*** dif "<<dif;
-
-    int ans = dif % 7;
-    //cout<<"\n*** ans "<<ans<<endl;
-
-    switch (ans) {
+	switch (ans) {
 	case 1:
-                cout<<"Tue";
-                break;
+		cout << "Tue";
+		break;
 	case 2:
-                cout<<"Wed";
-                break;
+		cout << "Wed";
+		break;
 	case 3:
-                cout<<"Thu";
-                break;
+		cout << "Thu";
+		break;
 	case 4:
-                cout<<"Fri";
-                break;
-	case 5: 		
-                cout<<"Sat";
-                break;
+		cout << "Fri";
+		break;
+	case 5:
+		cout << "Sat";
+		break;
 	case 6:
-                cout<<"Sun";
-                break;
+		cout << "Sun";
+		break;
 	default:
-                cout<<"Mon";
+		cout << "Mon";
 	}
 }
