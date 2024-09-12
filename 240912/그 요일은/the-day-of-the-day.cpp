73d9a1c12223d6dc8f7714b4447f307b5b74fa78
codingJokerr <126440492+codingJokerr@users.noreply.github.com>
2024-09-12 22:27:18 +0900

    Title: 그 요일은, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240912/그 요일은/the-day-of-the-day.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int days[13] = {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+int CalDays(int month, int day) {
+    int sum = day;
+    
+    for(int i=1; i<month; i++) {// 1월부터 직전 달까지
+        sum += days[i];
+    }
+
+	return sum;
+}
+
+int main() {
+    int m1,d1,m2,d2;    string checkDay;
+    cin>>m1>>d1>>m2>>d2>>checkDay;
+
+    int day1 = CalDays(m1, d1);
+    int day2 = CalDays(m2, d2);
+
+	// day1이 항상 앞선다.
+    int dif = day2 - day1;
+	if(dif < 7) 
+		cout << 0;
+	else {
+		string dayOfWeek[7] = {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
+		int idx = 0;
+		for(int i=0; i<7; i++) {
+			if(checkDay == dayOfWeek[i])
+				idx = i;
+		}
+    	// m1월 d1월은 월요일이다.
+    	// dif + 해당 요일 처리... +) -7??
+	
+    	int ans = (dif / 7) +1;	// 0인 경우가 없다.
+
+		cout<<ans;
+	}
+}
