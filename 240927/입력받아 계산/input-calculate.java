c0b5a9bf81bd4cfeb789377387df171e649dbba7
codingJokerr <126440492+codingJokerr@users.noreply.github.com>
2024-09-27 23:36:20 +0900

    Title: 입력받아 계산, Time: 120ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240927/입력받아 계산/input-calculate.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // sc 라는 이름이 Scanner의 역할을 하게된다.
+
+        int a;
+        a = sc.nextInt();
+        System.out.println(a+2);
+    }
+}
